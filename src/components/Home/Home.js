aca5decd8eef39810814f14de7c402a70639e083
Floalai <wikalai@gmail.com>
2021-09-14 13:54:28 +0200

    creando info seccion

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import Navbar from '../Navbar/Navbar';
 import HeroSection from '../HeroSection/HeroSection';
+import InfoSection from '../InfoSection/InfoSection';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState
@@ -16,6 +17,7 @@ const Home = () => {
          <Sidebar is0pen={isOpen} toggle={toggle} />
          <Navbar toggle={toggle} />
          <HeroSection />
+         <InfoSection />
         </div>
     );
 };
